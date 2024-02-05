8223d11d2578789beea27be035b308bb912b0a53
realmojo <strikers1999@naver.com>
2024-02-05 23:12:05 +0900

    Update controller.js

--- routes/downsoft/controller.js
+++ routes/downsoft/controller.js
@@ -2,7 +2,7 @@ const axios = require("axios");
 
 const doUploadS3 = async (req, res) => {
   try {
-    // const { location } = req.file;
+    const { location } = req.file;
     // const { idx } = req.query;
     // if (idx && location) {
     //   const param = {
@@ -12,7 +12,7 @@ const doUploadS3 = async (req, res) => {
     //   await axios.post("https://api.mindpang.com/api/test/update/logo/", param);
     //   res.status(200).send(location);
     // }
-    res.status(200).send("ok");
+    res.status(200).send(location);
   } catch (e) {
     console.log(e);
     return res.status(500).send(e);

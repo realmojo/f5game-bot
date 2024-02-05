b14d071c5972c02a4d13c7d8cc53f160e335a593
realmojo <strikers1999@naver.com>
2024-02-05 22:11:03 +0900

    Create controller.js

---
+++ routes/downsoft/controller.js
@@ -0,0 +1,24 @@
+const axios = require("axios");
+
+const doUploadS3 = async (req, res) => {
+  try {
+    // const { location } = req.file;
+    // const { idx } = req.query;
+    // if (idx && location) {
+    //   const param = {
+    //     idx,
+    //     logo: location,
+    //   };
+    //   await axios.post("https://api.mindpang.com/api/test/update/logo/", param);
+    //   res.status(200).send(location);
+    // }
+    res.status(200).send("ok");
+  } catch (e) {
+    console.log(e);
+    return res.status(500).send(e);
+  }
+};
+
+module.exports = {
+  doUploadS3,
+};

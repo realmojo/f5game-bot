339757ac467ce4df8b108511da0755a51b1f6bf5
realmojo <strikers1999@naver.com>
2024-06-22 21:29:49 +0900

    good

--- routes/techtoktok/common.js
+++ routes/techtoktok/common.js
@@ -8,10 +8,23 @@ const headers = {
     "Content-Type": "application/json",
   },
 };
+
+const getModels = async () => {
+  try {
+    const { data } = await axios.get(
+      "https://api.openai.com/v1/models",
+      headers
+    );
+    return data;
+  } catch (e) {
+    console.log(e);
+  }
+};
+
 // CHAT GPT API 요청 생성
 const generateBlogContent = async (topic) => {
   try {
-    const content = `[[주제:${topic}]] 글을 SEO맞게 작성해줘. 글은 4000자 이상으로 작성해줘. 형식은 마크다운 형식을 사용해줘. 대제목을 작성해주고 소제목도 5개 정도 넣어주고 소제목에 대한 상세한 설명은 1000자 이상으로 등을 이용해서 최대한 창의적으로 작성해줘. 마지막으로 마무리도 직접 작성해서 상세히 설명해줘.`;
+    const content = `[[주제:${topic}]] 글을 SEO맞게 작성해줘. 글은 4000자 이상으로 작성해줘. 형식은 마크다운 형식을 사용해줘. 대제목을 작성해주고 소제목도 3개 정도 넣어주고 소제목에 대한 상세한 설명은 1000자 이상으로 등을 이용해서 최대한 창의적으로 작성해줘. 마지막으로 마무리도 직접 작성해서 상세히 설명해줘.`;
 
     console.log(`주제 [${topic}]에 대해서 작성합니다..`);
     const { data } = await axios.post(
@@ -24,13 +37,13 @@ const generateBlogContent = async (topic) => {
             content,
           },
         ],
-        temperature: 0.2,
+        temperature: 1,
       },
       headers
     );
     return data;
   } catch (e) {
-    console.log(e);
+    console.log(e.data);
   }
 };
 
@@ -139,4 +152,5 @@ module.exports = {
   googleIndexingApi,
   generateBlogContent,
   getCategoryNumber,
+  getModels,
 };

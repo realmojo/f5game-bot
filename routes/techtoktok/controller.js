d6c51c74894221eabb85edce1a8bb4068b4a137b
realmojo <strikers1999@naver.com>
2024-06-22 22:14:07 +0900

    fdf

--- routes/techtoktok/controller.js
+++ routes/techtoktok/controller.js
@@ -31,7 +31,6 @@ cron.schedule("*/10 * * * *", async () => {
       await axios.get(
         `https://api.mindpang.com/api/dream/add.php?lastId=${nextIndex}`
       );
-      // return res.status(200).send(result);
     }
   } catch (e) {
     console.log(e);

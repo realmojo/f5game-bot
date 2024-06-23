eaf740a1a8b305c47fdb4bb3cfe2bbe7e818ce3d
realmojo <strikers1999@naver.com>
2024-06-23 14:19:47 +0900

    v

--- routes/techupbox/controller.js
+++ routes/techupbox/controller.js
@@ -15,7 +15,7 @@ cron.schedule("* * * * *", async () => {
   console.log(data);
 });
 
-// utc 시간 적용 +9 -> 24시 === 새벽 0시d
+// utc 시간 적용 +9 -> 24시 === 새벽 0시ddf
 cron.schedule("*/10 * * * *", async () => {
   try {
     const { data: autoItem } = await axios.get(

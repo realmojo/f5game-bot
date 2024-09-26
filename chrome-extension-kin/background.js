c3c708deda3f8843c5f8d487b7d5d0c035671275
realmojo <strikers1999@naver.com>
2024-09-26 23:05:19 +0900

    sf

--- chrome-extension-kin/background.js
+++ chrome-extension-kin/background.js
@@ -1,18 +1,43 @@
-const run = () => {
-  // console.log("back", chrome);
-  // chrome.cookies.get(
-  //   {
-  //     url: "https://naver.com",
-  //     name: "NID_AUT",
-  //   },
-  //   function (cookie) {
-  //     if (cookie) {
-  //       console.log("쿠키 값:", cookie.value);
-  //     } else {
-  //       console.log("쿠키를 찾을 수 없습니다.");
-  //     }
-  //   }
-  // );
+const setNaverCookie = async (NID_AUT, NID_SES) => {
+  try {
+    const data = {
+      NID_AUT,
+      NID_SES,
+    };
+
+    const response = await fetch(
+      "https://f5game-bot.vercel.app/techupbox/setNaverCookie",
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(data),
+      }
+    );
+    return response.json();
+  } catch (e) {
+    console.log("실패: ", e);
+  }
 };
+
+const run = async () => {
+  setInterval(async () => {
+    const NID_AUT = await chrome.cookies.get({
+      url: "https://naver.com",
+      name: "NID_AUT",
+    });
+    const NID_SES = await chrome.cookies.get({
+      url: "https://naver.com",
+      name: "NID_SES",
+    });
+    console.log("NID_AUT", NID_AUT.value);
+    console.log("NID_SES", NID_SES.value);
+    const d = await setNaverCookie(NID_AUT.value, NID_SES.value);
+    console.log(d);
+  }, 5000);
+};
+
+// 예제 사용
 
 run();

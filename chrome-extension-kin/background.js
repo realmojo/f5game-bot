21cbafa9746b6ee526c5eacd3021465d5d2805d5
JungManKyung <mk.jung01@gretail.com>
2024-09-27 16:36:47 +0900

    Update background.js

--- chrome-extension-kin/background.js
+++ chrome-extension-kin/background.js
@@ -1,6 +1,7 @@
-const setNaverCookie = async (NID_AUT, NID_SES) => {
+const setNaverCookie = async (CURRENT_ID, NID_AUT, NID_SES) => {
   try {
     const data = {
+      CURRENT_ID,
       NID_AUT,
       NID_SES,
     };
@@ -22,20 +23,25 @@ const setNaverCookie = async (NID_AUT, NID_SES) => {
 };
 
 const run = async () => {
-  setInterval(async () => {
-    const NID_AUT = await chrome.cookies.get({
-      url: "https://naver.com",
-      name: "NID_AUT",
-    });
-    const NID_SES = await chrome.cookies.get({
-      url: "https://naver.com",
-      name: "NID_SES",
-    });
-    console.log("NID_AUT", NID_AUT.value);
-    console.log("NID_SES", NID_SES.value);
-    const d = await setNaverCookie(NID_AUT.value, NID_SES.value);
-    console.log(d);
-  }, 5000);
+  // setInterval(async () => {
+  const NID_AUT = await chrome.cookies.get({
+    url: "https://naver.com",
+    name: "NID_AUT",
+  });
+  const NID_SES = await chrome.cookies.get({
+    url: "https://naver.com",
+    name: "NID_SES",
+  });
+  const CurrentId = await chrome.cookies.get({
+    url: "https://kin.naver.com",
+    name: "currentId",
+  });
+  console.log("NID_AUT", NID_AUT.value);
+  console.log("NID_SES", NID_SES.value);
+  console.log("CurrentId", CurrentId.value);
+  const d = await setNaverCookie(CurrentId.value, NID_AUT.value, NID_SES.value);
+  console.log(d);
+  // }, 10000);
 };
 
 // 예제 사용

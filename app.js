f65464403e9d289cfcf4ed3487a64e1c23e482fe
realmojo <strikers1999@naver.com>
2025-04-20 10:09:57 +0900

    gg

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ app.use(bodyParser.urlencoded({ limit: "50mb", extended: true }));
 app.use(bodyParser.json({ limit: "50mb" }));
 app.use(cors());
 app.use((req, res, next) => {
-  // console.log("origin: ", req.headers.origin);
+  console.log("origin: ", req.headers.origin);
   res.header("Access-Control-Allow-Origin", req.headers.origin); // 클라이언트의 정확한 도메인 설정
   res.header("Access-Control-Allow-Credentials", "true"); // 쿠키 허용
   res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");

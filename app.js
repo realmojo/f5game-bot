b450af29359b9fc0857c000829b31f04acd7a115
realmojo <strikers1999@naver.com>
2025-06-24 00:33:03 +0900

    ee

--- app.js
+++ app.js
@@ -5,18 +5,18 @@ const cors = require("cors");
 const server = require("http").createServer(app);
 const moment = require("moment");
 const port = process.env.PORT || 3001;
-
+console.log("port", port);
 server.setTimeout(500000);
 
 app.use(bodyParser.urlencoded({ limit: "50mb", extended: true }));
 app.use(bodyParser.json({ limit: "50mb" }));
 app.use(cors());
 app.use((req, res, next) => {
-  console.log("origin: ", req.headers.origin);
-  res.header("Access-Control-Allow-Origin", req.headers.origin); // 클라이언트의 정확한 도메인 설정
-  res.header("Access-Control-Allow-Credentials", "true"); // 쿠키 허용
-  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
-  res.header("Access-Control-Allow-Headers", "Content-Type");
+  // console.log("origin: ", req.headers.origin);
+  // res.header("Access-Control-Allow-Origin", req.headers.origin); // 클라이언트의 정확한 도메인 설정
+  // res.header("Access-Control-Allow-Credentials", "true"); // 쿠키 허용
+  // res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+  // res.header("Access-Control-Allow-Headers", "Content-Type");
   next();
 });
 app.use("/api", require("./routes/api"));
